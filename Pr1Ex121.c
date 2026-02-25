d71f39380b2a154af025ed132d34e2718571ace4
Marco <giraldahavana@.com>
2026-02-25 22:04:12 +0100

    Afegir t mínimes i gestionar memòria

--- Pr1Ex121.c
+++ Pr1Ex121.c
@@ -21,7 +21,7 @@ int main() {
   int ll;
   float *t_max, *t_min, *tm_max, *tm_min;
 
-  if ((meteo = fopen("./../MeteoCat2024.txt", "r")) == NULL) {
+  if ((meteo = fopen("MeteoCat2024.txt", "r")) == NULL) {
     printf("No es pot obrir el fitxer\n");
     return 1;
   }
@@ -54,38 +54,58 @@ int main() {
 
   fclose(meteo);
 
-  printf("   Comarca              Estacio                             Max\n");
-  printf("================================================================\n");
+  printf("   Comarca              Estacio                             Max         Min\n");
+  printf("============================================================================\n");
   for (i = 0; i < numcom; i++) {
     printf("%2u ", i + 1);
     printf("%-20s ", comarca[i].nom);
     printf("%-35s ", comarca[i].estacio);
     printf("%-.1f", comarca[i].tmx);
+    printf("\t%-.1f", comarca[i].tmn); // afegim les temperatures mínimes
     printf("\n");
   }
 
-  printf("\n Ara intentare codi propi");
+  // codi propi
+  // reserva de memòria
+  t_max = (float *)malloc(sizeof(float));
+  t_min = (float *)malloc(sizeof(float));
+  tm_max = (float *)malloc(sizeof(float));
+  tm_min = (float *)malloc(sizeof(float));
+  
+  p_max = (unsigned *)malloc(sizeof(unsigned));
+  p_min = (unsigned *)malloc(sizeof(unsigned));
+
+  // anunciem les funcions que utilitzarem
   maxmin(comarca, numcom, t_max, t_min, p_max, p_min);
   mitj_temperatures(comarca, numcom, tm_max, tm_min);
 
-  printf("La mitjana de temperatures maximes es: %.2f", *tm_max);
-  printf("La mitjana de temperatures minimes es: %.2f", *tm_min);
+  printf("\nLa mitjana de temperatures màximes (tm_max) és: %.2f °C\n", *tm_max);
+  printf("La mitjana de temperatures mínimes (tm_min) és: %.2f °C\n\n", *tm_min);
+  // alliberem els espais de memòria
+  free(t_max);
+  free(t_min);
+  free(tm_max);
+  free(tm_min);
+  free(p_max);
+  free(p_min);
+  free(comarca);
   return 0;
 }
-
+// definim la funció per calcular la mitjana de temperatures màximes i mínimes
 void mitj_temperatures(MetCom llista[], unsigned num_elements, float *tm_max, float *tm_min){
   unsigned j;
-  int suma_max = 0;
-  int suma_min = 0;
-  for (j = 0; j < num_elements; j++){
+  float suma_max = 0.;
+  float suma_min = 0.;
+  for (j = 0; j < num_elements; j++){ // amb un bulce acumulem la suma de les temperatures
     suma_max += llista[j].tmx;
     suma_min += llista[j].tmn;
   }
+  // fem la mitjana de les màximes i mínimes
   *tm_max = (suma_max / num_elements);
   *tm_min = (suma_min / num_elements);
 }
 
-
+// definim la funció per calcular la temperatura màxima
 void maxmin(MetCom llista[], unsigned num_elements, float *t_max, float *t_min, unsigned *p_max, unsigned *p_min){
   unsigned j;
   *t_min = llista[0].tmn;
@@ -93,11 +113,11 @@ void maxmin(MetCom llista[], unsigned num_elements, float *t_max, float *t_min, 
   *p_min = *p_max = 0;
   
   for (j=0; j < num_elements; j++){
-    if (llista[j].tmn < *t_min){
+    if (llista[j].tmn < *t_min){ // si la temperatura actual és menor que la mínima guardem aquesta com la nova mínima
       *t_min = llista[j].tmn;
       *p_min = j;
     }
-    if (llista[j].tmx > *t_max){
+    if (llista[j].tmx > *t_max){ // el mateix raonament amb les temperatures màximes
       *t_max = llista[j].tmx;
       *p_max = j;
     }
